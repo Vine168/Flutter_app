f88e30e2ee6855e6921a06028af92a9458e9365b
Vine168 <135112938+Vine168@users.noreply.github.com>
2025-02-26 10:40:07 +0700

    BLA-006  Add a Tween animation to show the Location Picker

--- lib/W3/utils/animations_util.dart
+++ lib/W3/utils/animations_util.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AnimationUtils {
-  static const int transitionSpeed = 500; //ms
+  static const int transitionSpeed = 500; // ms
 
-  ////
+  ///
   /// Slide given screen from bottom to top
   ///
   static Route<T> createBottomToTopRoute<T>(Widget screen) {
-    const begin = Offset(0.0, 0.0);         //  TODO
-    const end = Offset(0.0, 0.0);           //  TODO
+    const begin = Offset(0.0, 1.0); // Start from the bottom
+    const end = Offset(0.0, 0.0);   // End at normal position (top)
     return _createAnimatedRoute(screen, begin, end);
   }
 
   static Route<T> _createAnimatedRoute<T>(
       Widget screen, Offset begin, Offset end) {
     return PageRouteBuilder<T>(
-      transitionDuration:
-          const Duration(milliseconds: transitionSpeed), // Animation speed
+      transitionDuration: const Duration(milliseconds: transitionSpeed),
       pageBuilder: (context, animation, secondaryAnimation) => screen,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         var tween = Tween(begin: begin, end: end)
