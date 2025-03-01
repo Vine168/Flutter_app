17daea226369e212db76a3f4e10652361e9fb6d8
Vine168 <135112938+Vine168@users.noreply.github.com>
2025-03-01 19:40:31 +0700

    BLA-004 implement ride preferences form

--- lib/W3/theme/theme.dart
+++ lib/W3/theme/theme.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-
-///
-/// Definition of App colors.
-///
 class BlaColors {
   static Color primary            = const Color(0xFF00aff5);
 
@@ -47,36 +43,29 @@ class BlaColors {
 ///
 class BlaTextStyles {
   static TextStyle heading = TextStyle(fontSize: 28, fontWeight: FontWeight.w500);
-
+  static TextStyle title =  TextStyle(fontSize: 20, fontWeight: FontWeight.w400);
   static TextStyle body =  TextStyle(fontSize: 16, fontWeight: FontWeight.w400);
-
   static TextStyle label =  TextStyle(fontSize: 13, fontWeight: FontWeight.w400);
-
   static TextStyle button =  TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
 }
 
-
-
-///
 /// Definition of App spacings, in pixels.
 /// Bascially small (S), medium (m), large (l), extra large (x), extra extra large (xxl)
-///
+
 class BlaSpacings {
   static const double s = 12;
   static const double m = 16; 
   static const double l = 24; 
   static const double xl = 32; 
   static const double xxl = 40; 
-
   static const double radius = 16; 
   static const double radiusLarge = 24; 
 }
+class BlaSize {
+  static const double icon = 24;
+}
 
-
-
-///
 /// Definition of App Theme.
-///
 ThemeData appTheme =  ThemeData(
   fontFamily: 'Eesti',
   scaffoldBackgroundColor: Colors.white,
