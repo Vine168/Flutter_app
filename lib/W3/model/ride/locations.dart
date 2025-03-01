ad21aceb68407cbbce23ab1de9b37935fc092bbe
Vine168 <135112938+Vine168@users.noreply.github.com>
2025-03-01 19:59:39 +0700

    BLA-004 – Implement Ride Preferences Form

--- lib/W3/model/ride/locations.dart
+++ lib/W3/model/ride/locations.dart
@@ -1,46 +1,43 @@
 ///
-/// Enumation of available BlaBlaCar countries
+/// Enumeration of supported countries.
 ///
 enum Country {
   france('France'),
   uk('United Kingdom'),
   spain('Spain');
 
-  final String name;
+  final String displayName;
 
-  const Country(this.name);
+  const Country(this.displayName);
 }
 
 ///
-/// This model describes a location (city, street).
+/// Represents a geographical location.
 ///
 class Location {
-  final String name;
+  final String cityName;
   final Country country;
 
-  const Location({required this.name, required this.country});
+  const Location({required this.cityName, required this.country});
+  Location.copy(Location other)
+      : cityName = other.cityName,
+        country = other.country;
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    return other is Location && other.name == name && other.country == country;
+    return other is Location && other.cityName == cityName && other.country == country;
   }
-
   @override
-  int get hashCode => name.hashCode ^ country.hashCode;
-
+  int get hashCode => cityName.hashCode ^ country.hashCode;
   @override
-  String toString() {
-    return name;
-  }
+  String toString() => cityName;
 }
-
 ///
-/// This model describes a street.
+/// Represents a street within a location.
 ///
 class Street {
-  final String name;
-  final Location city;
-
-  const Street({required this.name, required this.city});
+  final String streetName;
+  final Location location;
+  const Street({required this.streetName, required this.location});
 }
--- lib/W3/screens/rides/rides_screen.dart
+++ lib/W3/screens/rides/rides_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/W3/screens/rides/widgets/ride_pref_bar.dart';
-
 import '../../model/ride/ride.dart';
 import '../../model/ride_pref/ride_pref.dart';
 import '../../service/rides_service.dart';
 import '../../theme/theme.dart';
 import 'widgets/rides_tile.dart';
-
 ///
 /// The Ride Selection screen allows users to select a ride once ride preferences have been defined.
 /// The screen also allows users to redefine the ride preferences and activate some filters.
